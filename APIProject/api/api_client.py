d56158eef238af20cd7e7449c387a401cd860c44
mmishina456 <Margarita.Mishina_ext@leroymerlin.ru>
2024-08-30 17:02:24 +0300

    Cover API methods by positive & negative tests

---
+++ APIProject/api/api_client.py
@@ -0,0 +1,21 @@
+import os
+
+from httpx import Client, Response
+
+from utilities.logger_utils import logger
+
+
+class ApiClient(Client):
+
+    def __init__(self):
+        super().__init__(base_url=f"https://{os.getenv('RESOURSE_URL')}")
+
+    def request(self, method, url, **kwargs) -> Response:
+        """
+        расширение логики метода httpx request с добавлением логирования типа запроса и его url,
+        логировать или нет задается в файле .env
+        """
+
+        if eval(os.getenv("USE_LOGS")):
+            logger.info(f'{method} {url}')
+        return super().request(method, url, **kwargs)
